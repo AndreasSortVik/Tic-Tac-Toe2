24e12ea1955747612c359c6231b7c6ddedde8a38
Andreas Sorteberg Vik <256838@stud.inn.no>
2021-09-21 08:51:07 +0200

    more functions

--- Tic-Tac-Toe/tic-tac-toe.cpp
+++ Tic-Tac-Toe/tic-tac-toe.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-void game();
-
 void player1Turn();
 void player2Turn();
-void tableBuild();
+
 void gameEnd();
 void gameRepeat();
+
+void tableBuild();
 void newTable();
 void clearcin();
 
@@ -14,7 +14,6 @@ char placement;
 
 bool player1 = true;
 bool player2 = true;
-bool repeat = false;
 
 const int row = 3;
 const int col = 3;
@@ -23,72 +22,33 @@ char table[row][col] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
 int main() {
 	std::cout << "******** Welcome to Tic-Tac-Toe! ********" << std::endl;
 	tableBuild();
-	repeat = true;
 
-	while (repeat == true) {
-		player1Turn();
+	while (true) {
+		while (true) {
+			player1Turn();
+			//Breaks out of the loop if player 1 won
+			if (player2 == false) {
+				//Makes sure player 1 starts first after a restarted match
+				player1 = true;
+				break;
+			}
 
-		if (player2 == false) {
-			repeat = false;
+			player2Turn();
+			//Breaks out of the loop if player 2 won
+			if (player1 == false) {
+				//Makes sure player 1 starts first after a restarted match
+				player1 = true;
+				break;
+			}
 		}
-		player2Turn();
 
-		if (repeat == false) {
-			std::cout << "******** Welcome to Tic-Tac-Toe! ********" << std::endl;
-			newTable();
-			repeat = true;
-		}
+		//Resets the layout after a win or a draw
+		std::cout << "******** Welcome to Tic-Tac-Toe! ********" << std::endl;
+		newTable();
 	}
 
 	return 0;
 }
-
-/*void game() {
-	while (true) {
-		while (player1 == true) {
-			//Asks where player 1 would like to place it's 'X'
-			std::cout << "Player 1's turn: ";
-			std::cin >> placement;
-			clearcin();
-
-			//Takes player 1's "placement" value and changes it's corresponding "table" value to an 'X'
-			for (int i = 0; i < row; i++) {
-				for (int j = 0; j < col; j++) {
-					if (placement == table[i][j]) {
-						table[i][j] = 'X';
-						tableBuild();
-					}
-				}
-			}
-
-			player2 = true;
-			player1 = false;
-			gameEnd();
-		}
-
-		while (player2 == true) {
-			//Asks where player 2 would like to place it's '0'
-			std::cout << "Player 2's turn: ";
-			std::cin >> placement;
-			clearcin();
-
-			//Takes player 2's "placement" value and changes it's corresponding "table" value to an '0'
-			for (int i = 0; i < row; i++) {
-				for (int j = 0; j < col; j++) {
-					if (placement == table[i][j]) {
-						table[i][j] = '0';
-						tableBuild();
-					}
-				}
-			}
-
-			player2 = false;
-			player1 = true;
-		}
-	}
-
-	return;
-}*/
 
 void player1Turn() {
 	while (player1 == true) {
@@ -134,21 +94,7 @@ void player2Turn() {
 
 		player2 = false;
 		player1 = true;
-	}
-
-	return;
-}
-
-//Builds the array table
-void tableBuild() {
-	//Prints the overhead line above the rest of the table
-	std::cout << std::endl << "---------------" << std::endl;
-
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			std::cout << "| " << table[i][j] << " |";
-		}
-		std::cout << std::endl << "---------------" << std::endl;
+		gameEnd();
 	}
 
 	return;
@@ -157,12 +103,84 @@ void tableBuild() {
 //Checks if any of the players got 3 in a row
 void gameEnd() {
 	if (table[0][0] == 'X' && table[0][1] == 'X' && table[0][2] == 'X') {
-		std::cout << "Gongrats player 1 won the game!" << std::endl;
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
 		player2 = false;
 		gameRepeat();
 	}
 	else if (table[1][0] == 'X' && table[1][1] == 'X' && table[1][2] == 'X') {
-		std::cout << "Gongrats player 1 won the game!" << std::endl;
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[2][0] == 'X' && table[2][1] == 'X' && table[2][2] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[0][0] == 'X' && table[1][0] == 'X' && table[2][0] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[0][1] == 'X' && table[1][1] == 'X' && table[2][1] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[0][2] == 'X' && table[1][2] == 'X' && table[2][2] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[0][0] == 'X' && table[1][1] == 'X' && table[2][2] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+	else if (table[0][2] == 'X' && table[1][1] == 'X' && table[2][0] == 'X') {
+		std::cout << "Gongrats Player 1 won the game!" << std::endl;
+		player2 = false;
+		gameRepeat();
+	}
+
+	else if (table[0][0] == '0' && table[0][1] == '0' && table[0][2] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[1][0] == '0' && table[1][1] == '0' && table[1][2] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[2][0] == '0' && table[2][1] == '0' && table[2][2] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[0][0] == '0' && table[1][0] == '0' && table[2][0] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[0][1] == '0' && table[1][1] == '0' && table[2][1] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[0][2] == '0' && table[1][2] == '0' && table[2][2] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[0][0] == '0' && table[1][1] == '0' && table[2][2] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
+		gameRepeat();
+	}
+	else if (table[0][2] == '0' && table[1][1] == '0' && table[2][0] == '0') {
+		std::cout << "Gongrats Player 2 won the game!" << std::endl;
+		player1 = false;
 		gameRepeat();
 	}
 
@@ -186,9 +204,26 @@ void gameRepeat() {
 			exit(0);
 		}
 		else {
+			system("cls");
 			std::cout << "Please enter a valid input (y/n). Try again.";
+			std::cout << std::endl;
 			continue;
 		}
+	}
+
+	return;
+}
+
+//Builds the array table
+void tableBuild() {
+	//Prints the overhead line above the rest of the table
+	std::cout << std::endl << "---------------" << std::endl;
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			std::cout << "| " << table[i][j] << " |";
+		}
+		std::cout << std::endl << "---------------" << std::endl;
 	}
 
 	return;
